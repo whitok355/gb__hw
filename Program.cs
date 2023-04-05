6c4976b24ddebf958f6295bd5d541851192c2c1d
whitok355 <3139139@mail.ru>
2023-04-05 16:29:02 +0300

    home work 8 sem

--- Program.cs
+++ Program.cs
@@ -1,83 +1,74 @@
-﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-// m = 3, n = 4.
-// 0,5 7 -2 -0,2
-// 1 -3,3 8 -9,9
-// 8 7,8 -7,1 9
-
-int row;
-int col;
-
-Console.Write("Введите число строк");
-int.TryParse(Console.ReadLine(), out row);
-Console.Write("Введите число столбцов");
-int.TryParse(Console.ReadLine(), out col);
-
-int[,] arr = new int[row, col];
-FillArr(arr);
-PrintArray(arr);
-void FillArr(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = new Random().Next(10);
-        }
-    }
-}
-
-void PrintArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for(int j = 0; j< array.GetLength(1); j++)
-        {
-            Console.Write(array[i,j]);
-        }
-        Console.WriteLine();
-    }
-}
-
-// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
 // 8 4 2 4
-// 17 -> такого числа в массиве нет
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
 
-int findRow, findCol;
-Console.Write("Введите строку искомого числа ");
-int.TryParse(Console.ReadLine(), out findRow);
-Console.Write("Введите столбец искомого числа ");
-int.TryParse(Console.ReadLine(), out findCol);
+// int row, col;
 
-int res = FindValue(arr, findRow-1, findCol-1);
+// Console.Write("Введите количество строк");
+// int.TryParse(Console.ReadLine(), out row);
+// Console.Write("Введите количество столбцов");
+// int.TryParse(Console.ReadLine(), out col);
 
-int FindValue(int[,] array, int row, int col)
-{
-    return array[row, col];
-}
+// int[,] arr = new int[row, col];
 
-Console.WriteLine(res);
+// FillArr();
+// PrintArr();
+// SortArr();
+// PrintArr();
 
-// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// void FillArr()
+// {
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             arr[i,j] = new Random().Next(10);
+//         }
+//     }
+// }
+
+// void SortArr()
+// {
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1) - 1; j++)
+//         {
+//             for (int z = 0; z < arr.GetLength(1) - 1; z++)
+//             {
+//                 if (arr[i, z] < arr[i, z + 1]) //для изменения сортировки поменять знак < на противоположный
+//                 {
+//                     int temp = 0;
+//                     temp = arr[i, z];
+//                     arr[i, z] = arr[i, z + 1];
+//                     arr[i, z + 1] = temp;
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// void PrintArr()
+// {
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int k = 0; k < arr.GetLength(1); k++)
+//         {
+//             Console.Write(arr[i,k]);
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
 // 8 4 2 4
-// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
-
-Calc(arr);
-
-void Calc(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        int res = 0;
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            res += array[i, j];
-        }
-        Console.WriteLine(res);
-    }
-}
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
